a662b4beb02b9d1e61b990648eefae9773b18a66
Md. Ashraful Hossen Akash <ashrafulhossainakash@gmail.com>
2026-01-02 18:17:32 +0600

    Add files via upload

---
+++ 1_printing_n_variables.py
@@ -0,0 +1,27 @@
+#this is my first python program
+print("I love pizza!")
+print("Pizza is the best food ever!")
+
+#variables as strings
+first_name="Ashraful"
+  
+print(first_name)
+print(f"Hello {first_name}")
+
+#integers
+age = 25
+quanity=3
+
+
+print(f"Your are buying {quanity} houses")
+
+#float
+price=19.99
+
+print(price)
+
+#boleans
+
+is_student = True
+
+print(f"Are you a student?{is_student}")
